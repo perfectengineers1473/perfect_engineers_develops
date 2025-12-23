2445d4b48ebf2e22fa4e21c59abc032bc580c68f
Hingu Prem <perfect.engineers1473@gmail.com>
2025-12-23 19:39:16 +0530

    last

--- sanity.config.ts
+++ sanity.config.ts
@@ -11,7 +11,8 @@ import {structureTool} from 'sanity/structure'
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './sanity/env'
 import {schema} from './sanity/schemaTypes'
-import {structure} from './sanity/structure'
+// FIXED: Import from the renamed file to avoid conflict
+import {structure} from './sanity/deskStructure' 
 
 export default defineConfig({
   basePath: '/studio',
