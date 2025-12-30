1da10c132c618e900fb04608bf6d2bd2434767b7
Hingu Prem <perfect.engineers1473@gmail.com>
2025-12-30 19:48:56 +0530

    solved

--- lib/utils/helperFunctions/index.ts
+++ lib/utils/helperFunctions/index.ts
@@ -1,6 +1,4 @@
 import { clsx, type ClassValue } from 'clsx'
-import { SANITY_WRITE_TOKEN } from '../../constants';
-import { getClient } from '../../sanity';
 
 export function cn(...inputs: ClassValue[]) {
     return clsx(inputs)
@@ -19,26 +17,4 @@ export const generateFormattedIndex = (index: number) => {
     const formattedIndex =
         updatedCount < 10 ? `0${updatedCount}` : updatedCount;
     return formattedIndex;
-};
-
-const client = getClient(SANITY_WRITE_TOKEN);
-
-export const uploadFiles = async (files: File[]) => {
-    try {
-        const uploadedFiles = await Promise.all(files?.map((file) => {
-            const asset = client?.assets?.upload('file', file);
-            return asset
-        }));
-        return uploadedFiles?.map(file => ({
-            _type: "file",
-            _key: file?._id,
-            asset: {
-                _type: "reference",
-                _ref: file?._id,
-            },
-        }))
-    } catch (error) {
-        console.error("Error uploading files:", error);
-        return [];
-    }
 };
